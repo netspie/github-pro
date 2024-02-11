cf4af506304556cf0e8789253d742b2297786426
drlsn <dariusz.lusnia@gmail.com>
2024-02-11 17:42:58 +0100

    Add test get repositories query handler and REST endpoint

--- src/main/kotlin/com/netspie/githubpro/GithubProApplication.kt
+++ src/main/kotlin/com/netspie/githubpro/GithubProApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class GithubproApplication
+class GithubProApplication
 
 fun main(args: Array<String>) {
-	runApplication<GithubproApplication>(*args)
+	runApplication<GithubProApplication>(*args)
 }
---
+++ src/main/kotlin/com/netspie/githubpro/repositories/RepositoriesController.kt
@@ -0,0 +1,16 @@
+package com.netspie.githubpro.repositories
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/repositories")
+class RepositoriesController(
+    private val getAllUserRepositoriesQueryHandler: GetAllUserRepositoriesQueryHandler
+) {
+    @GetMapping
+    fun getAll(username: String) =
+        getAllUserRepositoriesQueryHandler.execute(
+            GetAllUserRepositoriesQuery(username))
+}
