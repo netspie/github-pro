41b35f89487f0cf2c700e571403020fead1e84fd
drlsn <dariusz.lusnia@gmail.com>
2024-02-11 16:40:09 +0100

    Add initial Spring Boot project

---
+++ src/main/kotlin/com/netspie/githubpro/GithubproApplication.kt
@@ -0,0 +1,11 @@
+package com.netspie.githubpro
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GithubproApplication
+
+fun main(args: Array<String>) {
+	runApplication<GithubproApplication>(*args)
+}
---
+++ src/test/kotlin/com/netspie/githubpro/GithubproApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.netspie.githubpro
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GithubproApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
